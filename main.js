ddc91b026c54132989f0fa5396f13ee303113b09
Diego <diegopablo012@gmail.com>
2024-02-24 21:59:24 -0600

    feat: express
    Correct implementation of express

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 const express = require('express');
-const { readFile } = require('fs');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    readFile('public/index.html', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send('An error occurred')
-        }
-        res.send(data)
-    })
-})
+
+app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Server listening at http://127.0.0.1:${port}`)
-})
+    console.log(`Server listening at http://127.0.0.1:${port}`);
+});
