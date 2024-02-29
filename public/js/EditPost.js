d735c53aa2026a72b7bb1304740fe66981eee14c
Diego <diegopablo012@gmail.com>
2024-02-29 12:59:27 -0600

    feat: edit post
    now can edit an existent post

---
+++ public/js/EditPost.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form-write-post');
+    const urlParams = new URLSearchParams(window.location.search);
+    const postId = urlParams.get('postId');
+
+    if (postId) {
+        loadPostData(postId);
+    } else {
+        console.error('No se proporcionó el ID del post.');
+    }
+
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault(); 
+
+        const postData = {
+            title: document.getElementById('input-title').value,
+            banner_image_url: document.getElementById('input-image').value,
+            category: document.getElementById('input-category').value,
+            content: document.getElementById('input-content').value,
+        };
+
+        try {
+            const response = await fetch(`http://127.0.0.1:3000/posts/${postId}`, {
+                method: 'PUT', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(postData),
+            });
+
+            if (response.ok) {
+                alert('Post actualizado exitosamente!');
+                window.location.href = '../index.html';
+            } else {
+                throw new Error('Error actualizando el post');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('Error al actualizar el post. Por favor, inténtalo de nuevo.');
+        }
+    });
+});
+
+async function loadPostData(postId) {
+    try {
+        const response = await fetch(`http://127.0.0.1:3000/posts/${postId}`);
+        const posts = await response.json(); 
+
+        posts.forEach(post => {
+            document.getElementById('input-title').value = post.title;
+            document.getElementById('input-image').value = post.banner_image_url;
+            document.getElementById('input-category').value = post.category;
+            document.getElementById('input-content').value = post.content;
+        });
+
+
+    } catch (error) {
+        console.error('Error cargando el post:', error);
+    }
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const postsContainer = document.getElementById('post-list');
+    const formSearchPost = document.getElementById('form-search-post');
+    const inputIdPost = document.getElementById('input-id-post');
 
     async function loadPosts() {
         try {
@@ -8,7 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
             posts = posts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
-            console.log(posts);
 
             while (postsContainer.children.length > 1) {
                 postsContainer.removeChild(postsContainer.lastChild);
@@ -20,12 +21,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 postElement.innerHTML = `
                     <h3 class="post-title"><span class="post-id">${post.id}</span>. ${post.title}</h3>
-                    <p class="post-date">${post.created_at}</p>
+                    <p class="post-date">${new Date(post.created_at)}</p>
                     <img src="${post.banner_image_url}" alt="" class="post-img">
                     <p class="post-category">${post.category}</p>
                     <p class="post-content">${post.content}</p>
                     <div class="post-actions">
-                        <button class="btn-edit-post">Editar</button>
+                        <button class="btn-edit-post" onclick="location.href='../html/EditPost.html?postId=${post.id}'">Editar</button>
                         <button class="btn-delete-post">Eliminar</button>
                     </div>
                 `;
@@ -38,7 +39,64 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     loadPosts();
+
+    formSearchPost.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        const postId = inputIdPost.value.trim();
+
+        if (!postId) {
+            alert('Por favor, ingresa un ID válido.');
+            return;
+        }
+
+        try {
+            const response = await fetch(`http://127.0.0.1:3000/posts/${postId}`);
+            if (!response.ok) {
+                if (response.status === 404) {
+                    alert('Post no encontrado.');
+                } else {
+                    throw new Error('Error en la búsqueda del post.');
+                }
+            }
+
+            const posts = await response.json();
+
+            posts.forEach(post => {
+                displayPost(post);
+            });
+            
+            
+        } catch (error) {
+            console.error('Error fetching post:', error);
+            alert('Error al buscar el post. Por favor, inténtalo de nuevo.');
+        }
+    });
+
 });
+
+function displayPost(post) {
+    const postsContainer = document.getElementById('post-list');
+    postsContainer.innerHTML = ''; 
+
+
+    const postElement = document.createElement('article');
+    postElement.classList.add('post-container');
+
+    postElement.innerHTML = `
+        <h3 class="post-title"><span class="post-id">${post.id}</span>. ${post.title}</h3>
+        <p class="post-date">${post.created_at}</p> <!-- Asegúrate de ajustar según la estructura de tu post -->
+        <img src="${post.banner_image_url}" alt="" class="post-img">
+        <p class="post-category">${post.category}</p>
+        <p class="post-content">${post.content}</p>
+        <div class="post-actions">
+            <button class="btn-edit-post">Editar</button>
+            <button class="btn-delete-post">Eliminar</button>
+        </div>
+    `;
+
+    postsContainer.appendChild(postElement);
+}
 
 
 
