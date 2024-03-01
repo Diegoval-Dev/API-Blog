99f4a96e0215a71366f1ebced3b4dcd041939136
Diego <diegopablo012@gmail.com>
2024-02-29 21:08:24 -0600

    ref: port

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 22309;
+const port = 3000;
 
 app.use(express.static(join(__dirname, 'public')));
 app.use(logDetails);
