593c7c4cb2601d6bbcc1f2beb8edaf73ba3df416
Diego <diegopablo012@gmail.com>
2024-03-05 09:27:16 -0600

    ref: Eslint
    Refractor db.js for EsLint standars

--- database/db.js
+++ database/db.js
@@ -1,26 +1,26 @@
-import conn from './conn.js'
+import conn from './conn'
 
-export async function getAllPosts() {
+export const getAllPosts = async () => {
   const [rows] = await conn.query('SELECT * FROM blog_posts')
   return rows
 }
 
-export async function createPost(title, content, banner_image_url, category) {
-  const [result] = await conn.query('INSERT INTO blog_posts (title, content, banner_image_url, category) VALUES (?, ?, ?, ?)', [title, content, banner_image_url, category])
+export const createPost = async (title, content, bannerImageUrl, category) => {
+  const [result] = await conn.query('INSERT INTO blog_posts (title, content, banner_image_url, category) VALUES (?, ?, ?, ?)', [title, content, bannerImageUrl, category])
   return result
 }
 
-export async function getPostById(id) {
+export const getPostById = async (id) => {
   const [rows] = await conn.query('SELECT * FROM blog_posts WHERE id = ?', [id])
   return rows
 }
 
-export async function updatePostById(id, title, content, banner_image_url, category) {
-  const [result] = await conn.query('UPDATE blog_posts SET title = ?, content = ?, banner_image_url = ?, category = ? WHERE id = ?', [title, content, banner_image_url, category, id])
+export const updatePostById = async (id, title, content, bannerImageUrl, category) => {
+  const [result] = await conn.query('UPDATE blog_posts SET title = ?, content = ?, banner_image_url = ?, category = ? WHERE id = ?', [title, content, bannerImageUrl, category, id])
   return result
 }
 
-export async function deletePostById(id) {
+export const deletePostById = async (id) => {
   const [result] = await conn.query('DELETE FROM blog_posts WHERE id = ?', [id])
   return result
 }
