2a34029cdae71d01312f9dae05f28d9c6d2f0fe4
Diego <diegopablo012@gmail.com>
2024-03-05 20:26:37 -0600

    ref: endpoints
    refractor endpoints

--- public/js/WritePost.js
+++ public/js/WritePost.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     try {
-      const response = await fetch('http://127.0.0.1:3000/posts', {
+      const response = await fetch('https://22309.arpanetos.lol//posts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
