ffadc11a943af9cb97bd37fcbabf30c953fd5aa7
Diego <diegopablo012@gmail.com>
2024-03-09 22:30:22 -0600

    ref: package structure
    Refractor package structure using best practices from FreeCodeCamp

---
+++ src/v1/routes/swagger.js
@@ -0,0 +1,19 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Blog API',
+      version: '1.0.0',
+      description: 'A simple Express API for a blog',
+    },
+    servers: [
+    {
+    url: 'http://localhost:3000',
+        },
+        ],
+    },
+    apis: ['./routes/*.js'],
+}
